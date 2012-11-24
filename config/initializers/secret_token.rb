1599dd8e2888875b1f22f1eb540277057d0654ac
Francisco <francisco000@gmail.com>
2012-11-24 12:22:28 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Politicos::Application.config.secret_token = '056b271d406d1109c7c2aaca7281d8df396c36241572140d95e7343ec67765b1afaf2bd4ad7fe89e7278afac30ce4b6a7b32e3066cc8c35a8acd06b5e9d7521c'
